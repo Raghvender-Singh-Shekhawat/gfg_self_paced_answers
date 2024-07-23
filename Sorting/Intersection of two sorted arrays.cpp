94b7e84512521daf5ddff39c4fbff6eab5b36e49
Raghvender Singh Shekhawat <115393957+Raghvender-Singh-Shekhawat@users.noreply.github.com>
2024-07-24 00:53:26 +0530

    Update Intersection of two sorted arrays.cpp
    Correct Code :D

--- Sorting/Intersection of two sorted arrays.cpp
+++ Sorting/Intersection of two sorted arrays.cpp
@@ -1,30 +1,43 @@
 class Solution {
-public:
-    vector<int> printIntersection(int arr1[], int arr2[], int N, int M) {
-        vector<int> result;
+  public:
+    // Function to return a list containing the intersection of two arrays.
+    vector<int> printIntersection(vector<int> arr1, vector<int> arr2) {
         int i = 0, j = 0;
-        while (i < N && j < M) {
-            if (i > 0 && arr1[i] == arr1[i - 1]) {
+        bool flag = false;
+        std::vector<int> v;
+        int n = arr1.size();
+        int m = arr2.size();
+        // Using two pointers i and j over the two arrays which helps
+        // in storing the elements which are present in both the arrays.
+        while (i < n && j < m) {
+            // Updating the pointer i if we have identical
+            // elements at consecutive position in arr1.
+            if (i > 0 && arr1[i - 1] == arr1[i]) {
                 i++;
                 continue;
             }
-            if (j > 0 && arr2[j] == arr2[j - 1]) {
-                j++;
-                continue;
-            }
-            if (arr1[i] == arr2[j]) {
-                result.push_back(arr1[i]);
+
+            // Comparing element of the arrays arr1 and arr2 at pointers
+            // i and j and storing only the elements which are present in
+            // both the arrays and updating the pointers.
+            if (arr1[i] < arr2[j])
                 i++;
+            else if (arr2[j] < arr1[i])
                 j++;
-            } else if (arr1[i] < arr2[j]) {
+            else {
+                v.push_back(arr2[j]);
+                flag = true;
                 i++;
-            } else {
                 j++;
             }
         }
-        if (result.empty()) {
-            result.push_back(-1);
-        }
-        return result;
+
+        // If no common element is present in both arrays,
+        // we store -1 in the list.
+        if (flag == false)
+            v.push_back(-1);
+
+        // returning the list containing the intersection of the two arrays.
+        return v;
     }
 };
